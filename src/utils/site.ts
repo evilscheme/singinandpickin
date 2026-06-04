1e35ae37b9e83dc825f0a5464b89a2d3b19cc661
Bryan Burns <bryanburns@gmail.com>
2026-06-04 14:39:55 -0700

    add playlist link

---
+++ src/utils/site.ts
@@ -0,0 +1,5 @@
+/** Site-wide constants and external links. */
+
+/** Apple Music playlist of every song we've picked. */
+export const appleMusicPlaylistUrl =
+  'https://music.apple.com/us/playlist/singing-and-picking/pl.u-yZJGXsBpB5y';
